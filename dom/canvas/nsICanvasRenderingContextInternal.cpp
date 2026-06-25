ab19d9833512f1a9b296a022ea5f2e5a0ca46c85
Emilio Cobos Álvarez <emilio@crisal.io>
2026-06-25 02:41:22 +0000

    Bug 2045604 - Don't leak the static doc via the relevant global of the context.  a=RyanVM DONTBUILD
    Original Revision: https://phabricator.services.mozilla.com/D306772
    
    Differential Revision: https://phabricator.services.mozilla.com/D307726

--- dom/canvas/nsICanvasRenderingContextInternal.cpp
+++ dom/canvas/nsICanvasRenderingContextInternal.cpp
@@ -28,6 +28,9 @@ mozilla::PresShell* nsICanvasRenderingContextInternal::GetPresShell() {
 
 nsIGlobalObject* nsICanvasRenderingContextInternal::GetParentObject() const {
   if (mCanvasElement) {
+    if (auto* original = mCanvasElement->GetOriginalCanvas()) {
+      return original->OwnerDoc()->GetScopeObject();
+    }
     return mCanvasElement->OwnerDoc()->GetScopeObject();
   }
   if (mOffscreenCanvas) {
