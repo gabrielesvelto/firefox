32530d68a354c0f156a0c668cae9e914e6e58c5c
giorga <giorga@mozilla.com>
2026-07-23 06:09:29 -0400

    Bug 2038713 - [WebCompat Reporter v3] Update "Preview report" bottom sheet. a=dsmith
    Original Revision: https://phabricator.services.mozilla.com/D308808
    
    Differential Revision: https://phabricator.services.mozilla.com/D313690

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/webcompat/ui/WebCompatReporter.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/webcompat/ui/WebCompatReporter.kt
@@ -218,7 +218,7 @@ private fun WebCompatReporterDialogs(
 
     if (previewSheetVisible) {
         WebCompatReporterPreviewSheet(
-            previewJSON = state.previewJSON,
+            previewReporterItems = state.previewReporterItems,
             onDismissRequest = onDismissPreview,
             onSendClick = { onAction(WebCompatReporterAction.SendReportClicked) },
             isSendButtonEnabled = state.isSubmitEnabled,
