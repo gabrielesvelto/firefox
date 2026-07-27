820c568a9165e8bd1c1459a7c190191b68b5dfb7
ffxbld <ffxbld@mozilla.com>
2026-07-27 15:18:00 +0000

    No Bug, mozilla-esr140 repo-update HSTS HPKP remote-settings tld-suffixes ct-logs -  a=diannaS DONTBUILD
    Differential Revision: https://phabricator.services.mozilla.com/D314306

--- security/ct/CTKnownLogs.h
+++ security/ct/CTKnownLogs.h
@@ -14,7 +14,7 @@
 
 #include <stddef.h>
 
-static const PRTime kCTExpirationTime = INT64_C(1790587104000000);
+static const PRTime kCTExpirationTime = INT64_C(1791186914000000);
 
 namespace mozilla::ct {
 
--- security/manager/ssl/StaticHPKPins.h
+++ security/manager/ssl/StaticHPKPins.h
@@ -681,4 +681,4 @@ static const TransportSecurityPreload kPublicKeyPinningPreloadList[] = {
 
 static const int32_t kUnknownId = -1;
 
-static const PRTime kPreloadPKPinsExpirationTime = INT64_C(1793006282214000);
+static const PRTime kPreloadPKPinsExpirationTime = INT64_C(1793606087472000);
