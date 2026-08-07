caeeec47abfb92c5adce0957396b006c5ea013ef
ffxbld <ffxbld@mozilla.com>
2026-08-07 20:35:17 +0000

    No Bug, mozilla-beta repo-update HSTS HPKP -  a=diannaS
    Differential Revision: https://phabricator.services.mozilla.com/D317147

--- security/manager/ssl/StaticHPKPins.h
+++ security/manager/ssl/StaticHPKPins.h
@@ -681,4 +681,4 @@ static const TransportSecurityPreload kPublicKeyPinningPreloadList[] = {
 
 static const int32_t kUnknownId = -1;
 
-static const PRTime kPreloadPKPinsExpirationTime = INT64_C(1793998244069000);
+static const PRTime kPreloadPKPinsExpirationTime = INT64_C(1794599306739000);
