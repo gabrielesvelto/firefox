95281e195392879e9a0f87ceac72d7198318659f
Jon Coppeard <jcoppeard@mozilla.com>
2026-08-07 17:07:42 +0000

    Bug 2051882 - Part 1: Fix handling of budget expiry during main thread only marking  a=diannaS
    Currently we don't take account of the fact that we may run out of budget doing
    main-thread-only marking at the start of a marking slice and return Finished
    when we're not.
    
    Original Revision: https://phabricator.services.mozilla.com/D310035
    
    Differential Revision: https://phabricator.services.mozilla.com/D317020

--- js/src/gc/GC.cpp
+++ js/src/gc/GC.cpp
@@ -3564,7 +3564,7 @@ bool GCRuntime::initMultiThreadedMarkers() {
   return true;
 }
 
-inline IncrementalProgress ToIncrementalProgress(bool finished) {
+static inline IncrementalProgress ToIncrementalProgress(bool finished) {
   return finished ? Finished : NotFinished;
 }
 
@@ -3573,18 +3573,24 @@ IncrementalProgress GCRuntime::markPhase(SliceBudget& budget) {
 
   markSliceCount++;
 
-  finishAnyConcurrentMarking(budget);
+  bool finishedMainThreadOnlyMarking = finishAnyConcurrentMarking(budget);
 
   auto [mainThreadBudget, helperThreadBudget] = budgetConcurrentMarking(budget);
 
-  markSynchronously(mainThreadBudget, useParallelMarking);
+  IncrementalProgress result =
+      markSynchronously(mainThreadBudget, useParallelMarking);
 
-  if (hasMarkingWork()) {
+  if (!marker().isMarkStackEmpty()) {
+    MOZ_ASSERT(result == NotFinished);
     maybeStartConcurrentMarking(helperThreadBudget);
     return NotFinished;
   }
 
-  return Finished;
+  if (!finishedMainThreadOnlyMarking) {
+    return NotFinished;
+  }
+
+  return result;
 }
 
 IncrementalProgress GCRuntime::markSynchronously(
@@ -3614,11 +3620,7 @@ IncrementalProgress GCRuntime::markSynchronously(
     MOZ_ASSERT(reportTime);
     MOZ_ASSERT(!isBackgroundMarking());
 
-    if (!ParallelMarker::mark(this, sliceBudget)) {
-      return NotFinished;
-    }
-
-    return Finished;
+    return ToIncrementalProgress(ParallelMarker::mark(this, sliceBudget));
   }
 
   return ToIncrementalProgress(
@@ -3632,11 +3634,7 @@ bool GCRuntime::hasMarkingWork() const {
     }
   }
 
-  if (hasDelayedMarking()) {
-    return true;
-  }
-
-  return false;
+  return hasDelayedMarking();
 }
 
 void GCRuntime::drainMarkStack() {
@@ -4278,11 +4276,11 @@ void GCRuntime::maybeStartConcurrentMarking(SliceBudget& budget) {
 #endif
 }
 
-void GCRuntime::finishAnyConcurrentMarking(JS::SliceBudget& budget) {
+bool GCRuntime::finishAnyConcurrentMarking(JS::SliceBudget& budget) {
 #ifdef JS_GC_CONCURRENT_MARKING
   if (!useConcurrentMarking) {
     MOZ_ASSERT(!isBackgroundMarking());
-    return;
+    return true;
   }
 
   pauseBackgroundMarking();
@@ -4294,13 +4292,20 @@ void GCRuntime::finishAnyConcurrentMarking(JS::SliceBudget& budget) {
   }
 
   // Perform as much main-thread-only marking as we can within the budget.
-  concurrentMarker().processMainThreadBuffers(budget);
+  bool result = concurrentMarker().processMainThreadBuffers(budget);
 
   GCMarker::moveAllWork(&marker(), &concurrentMarker());
 
   if (!canMarkConcurrently()) {
+    // Abort concurrent marking. Ensure main thread buffers are traced first.
+    SliceBudget unlimitedBudget = SliceBudget::unlimited();
+    concurrentMarker().processMainThreadBuffers(unlimitedBudget);
     useConcurrentMarking = NoConcurrentMarking;
   }
+
+  return result;
+#else
+  return true;
 #endif
 }
 
