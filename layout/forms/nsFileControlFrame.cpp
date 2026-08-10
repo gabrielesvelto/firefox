7e24400885af55c54e6b5d99f255641a5726c021
Dianna Smith <dsmith@mozilla.com>
2026-08-10 06:30:12 -0400

    Revert "Bug 2057204 - use the right AddSystemEventListener variant with nsFileControlFrame::mMouseListener," a=backout
    This reverts commit 4a735600228283ee786fdf06c241c257870e3ed1.

--- layout/forms/nsFileControlFrame.cpp
+++ layout/forms/nsFileControlFrame.cpp
@@ -140,9 +140,8 @@ nsresult nsFileControlFrame::CreateAnonymousContent(
   aElements.AppendElement(mTextContent);
 
   // We should be able to interact with the element by doing drag and drop.
-  mContent->AddSystemEventListener(u"drop"_ns, mMouseListener, false, false);
-  mContent->AddSystemEventListener(u"dragover"_ns, mMouseListener, false,
-                                   false);
+  mContent->AddSystemEventListener(u"drop"_ns, mMouseListener, false);
+  mContent->AddSystemEventListener(u"dragover"_ns, mMouseListener, false);
 
   SyncDisabledState();
 
