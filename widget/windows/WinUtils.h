ed02424296d311aca1d48eb6962ea85c2d658aeb
David Parks <daparks@mozilla.com>
2026-08-14 02:37:10 +0000

    Bug 2057108: Cleanup DirectManipulation initialization failure on Windows  a=RyanVM DONTBUILD
    Ensures we properly tear down services on failure.  Also promotes
    NS_ENSURE_HRESULT[_VOID] to WinUtils.
    
    Original Revision: https://phabricator.services.mozilla.com/D313871
    
    Differential Revision: https://phabricator.services.mozilla.com/D317690

--- widget/windows/WinUtils.h
+++ widget/windows/WinUtils.h
@@ -12,6 +12,7 @@
 #include <uxtheme.h>
 #include <dwmapi.h>
 #include <unordered_map>
+#include <cinttypes>
 #include <utility>
 
 // Undo the windows.h damage
@@ -38,11 +39,44 @@
 #include "mozilla/Attributes.h"
 #include "mozilla/EventForwards.h"
 #include "mozilla/LazyIdleThread.h"
+#include "mozilla/Printf.h"
 #include "mozilla/UniquePtr.h"
 #include "mozilla/Vector.h"
 #include "mozilla/WindowsDpiAwareness.h"
 #include "mozilla/WindowsProcessMitigations.h"
 #include "mozilla/gfx/2D.h"
+
+#ifdef DEBUG
+#  define NS_ENSURE_HRESULT(hres, ret)                    \
+    do {                                                  \
+      HRESULT result = hres;                              \
+      if (MOZ_UNLIKELY(FAILED(result))) {                 \
+        mozilla::SmprintfPointer msg = mozilla::Smprintf( \
+            "NS_ENSURE_HRESULT(%s, %s) failed with "      \
+            "result 0x%" PRIX32,                          \
+            #hres, #ret, static_cast<uint32_t>(result));  \
+        NS_WARNING(msg.get());                            \
+        return ret;                                       \
+      }                                                   \
+    } while (false)
+#  define NS_ENSURE_HRESULT_VOID(hres)                    \
+    do {                                                  \
+      HRESULT result = hres;                              \
+      if (MOZ_UNLIKELY(FAILED(result))) {                 \
+        mozilla::SmprintfPointer msg = mozilla::Smprintf( \
+            "NS_ENSURE_HRESULT(%s) failed with "          \
+            "result 0x%" PRIX32,                          \
+            #hres, static_cast<uint32_t>(result));        \
+        NS_WARNING(msg.get());                            \
+        return;                                           \
+      }                                                   \
+    } while (false)
+#else
+#  define NS_ENSURE_HRESULT(hres, ret) \
+    if (MOZ_UNLIKELY(FAILED(hres))) return ret
+#  define NS_ENSURE_HRESULT_VOID(hres) \
+    if (MOZ_UNLIKELY(FAILED(hres))) return
+#endif
 
 /**
  * NS_INLINE_DECL_IUNKNOWN_REFCOUNTING should be used for defining and
