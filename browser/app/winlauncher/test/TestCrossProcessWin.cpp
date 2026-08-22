7997be6418e3aa2c4f8a91ffefac9ebb47de28e1
Ryan VanderMeulen <rvandermeulen@mozilla.com>
2026-08-22 09:54:04 -0400

    Revert "Bug 2059027 - Create the DLL blocklist shared section with an empty DACL. a=RyanVM DONTBUILD" for TestCrossProcessWin failures.
    This reverts commit cae4777a10b67948245e50e047deb6c7375db6f6.

--- browser/app/winlauncher/test/TestCrossProcessWin.cpp
+++ browser/app/winlauncher/test/TestCrossProcessWin.cpp
@@ -70,7 +70,6 @@ class SharedSectionTestHelper {
            (offsetof(SharedSection::Layout, mFirstBlockEntry) +
             sizeof(DllBlockInfo));
   }
-  static HANDLE GetSectionHandle() { return SharedSection::sSectionHandle; }
 };
 }  // namespace mozilla::freestanding
 
@@ -513,18 +512,6 @@ class ChildProcess final {
     if (result.inspectErr() !=
         WindowsError::FromWin32Error(ERROR_ACCESS_DENIED)) {
       PrintLauncherError(result, "The readonly section was writable");
-      return 1;
-    }
-
-    // The empty DACL should prevent writable handles.
-    HANDLE writableHandle;
-    if (::DuplicateHandle(
-            nt::kCurrentProcess, SharedSectionTestHelper::GetSectionHandle(),
-            nt::kCurrentProcess, &writableHandle, GENERIC_WRITE, FALSE, 0)) {
-      ::CloseHandle(writableHandle);
-      printf(
-          "TEST-FAILED | TestCrossProcessWin | "
-          "The handle was writable.\n");
       return 1;
     }
 
