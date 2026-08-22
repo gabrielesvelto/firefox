cb3a39085bb1a0968ed2e7f2bbe6d3c13925f0e2
Yubin Jamora <yjamora@mozilla.com>
2026-08-22 14:27:24 +0000

    Bug 2062573 - Execute the create task form on Enter key in Smart Window  a=RyanVM
    Original Revision: https://phabricator.services.mozilla.com/D318178
    
    Differential Revision: https://phabricator.services.mozilla.com/D320501

--- browser/components/urlbar/content/SmartbarInputUtils.mjs
+++ browser/components/urlbar/content/SmartbarInputUtils.mjs
@@ -558,16 +558,13 @@ function setupCommandsPlugin(editorElement, panelList) {
     }
   };
 
-  const completeCommand = id => {
+  // Selecting a command runs it immediately
+  const executeCommand = (id, submitType) => {
     if (!latestCommandData) {
       return;
     }
-    const commandText = `/${id} `;
-    const { view, range } = latestCommandData;
-    view.dispatch(view.state.tr.insertText(commandText, range.from, range.to));
-    view.focus();
-
     onExitPalette();
+    smartbarInput.submitChat(null, `/${id}`, submitType);
   };
 
   const handleItemSelected = e => {
@@ -578,7 +575,7 @@ function setupCommandsPlugin(editorElement, panelList) {
     ) {
       return;
     }
-    completeCommand(e.detail.id);
+    executeCommand(e.detail.id, "button");
   };
 
   const handlePanelKeyDown = e =>
@@ -601,7 +598,7 @@ function setupCommandsPlugin(editorElement, panelList) {
       Enter: () => {
         const selected = panelList.getSelectedItem();
         if (selected) {
-          completeCommand(selected.id);
+          executeCommand(selected.id, "enter");
         }
       },
       Escape: () => onExitPalette(),
