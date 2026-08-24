3b5be5277d9ac1584d0d3c323ecc103ef0a4c921
Kaitlyn <kandres@mozilla.com>
2026-08-24 00:20:58 +0000

    Bug 2065206 - Disable trackerCount by default; it will be enabled via Experimenter instead.  a=RyanVM
    We’re going to set the pref to false by default for the Show Trackers feature, since we’ll be using Experimenter to enable it for users instead. This approach is also safer for rollout, as it gives us full control over the feature’s visibility. If anything goes wrong, we can simply disable the experiment and have the feature remain off by default.
    
    Original Revision: https://phabricator.services.mozilla.com/D320216
    
    Differential Revision: https://phabricator.services.mozilla.com/D320764

--- browser/app/profile/firefox.js
+++ browser/app/profile/firefox.js
@@ -486,8 +486,8 @@ pref("browser.urlbar.deduplication.enabled", true);
 
 pref("browser.urlbar.scotchBonnet.enableOverride", true);
 
-pref("browser.urlbar.trackerCount.featureGate", true);
-pref("browser.urlbar.trackerCount.enabled", true);
+pref("browser.urlbar.trackerCount.featureGate", false);
+pref("browser.urlbar.trackerCount.enabled", false);
 
 pref("browser.urlbar.trustPanel.featureGate", true);
 pref("browser.urlbar.trustPanel.breachAlerts.featureGate", true);
