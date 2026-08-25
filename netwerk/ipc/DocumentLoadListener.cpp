af6a97fed28c6f4084a00b03ee3042aa86d6c565
Valentin Gosu <valentin.gosu@gmail.com>
2026-08-25 19:53:27 +0000

    Bug 2054650 - Enforce TYPE_INTERNAL_OBJECT/EMBED content policy types for object loads  a=RyanVM DONTBUILD
    Original Revision: https://phabricator.services.mozilla.com/D320111
    
    Differential Revision: https://phabricator.services.mozilla.com/D321245

--- netwerk/ipc/DocumentLoadListener.cpp
+++ netwerk/ipc/DocumentLoadListener.cpp
@@ -49,6 +49,7 @@
 #include "nsExternalHelperAppService.h"
 #include "nsHttpChannel.h"
 #include "nsIBrowser.h"
+#include "nsIContentPolicy.h"
 #include "nsIHttpChannelInternal.h"
 #include "nsIStreamConverterService.h"
 #include "nsIViewSourceChannel.h"
@@ -1098,6 +1099,15 @@ auto DocumentLoadListener::OpenObject(
        aLoadState->URI()->GetSpecOrDefault().get()));
 
   MOZ_ASSERT(!mIsDocumentLoad);
+
+  // The content policy type is child-controlled; object loads must only
+  // ever claim to be object or embed loads, as the parent bases
+  // type-keyed security decisions (cookies, third-party status) on it.
+  if (aContentPolicyType != nsIContentPolicy::TYPE_INTERNAL_OBJECT &&
+      aContentPolicyType != nsIContentPolicy::TYPE_INTERNAL_EMBED) {
+    *aRv = NS_ERROR_UNEXPECTED;
+    return nullptr;
+  }
 
   auto sandboxFlags = aLoadState->TriggeringSandboxFlags();
 
