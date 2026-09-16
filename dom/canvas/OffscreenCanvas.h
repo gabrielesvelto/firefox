29392b30b726b3417472fb324329971df3a285db
Lee Salzman <lsalzman@mozilla.com>
2026-09-16 10:35:52 +0000

    Bug 2068375.  a=pascalc DONTBUILD
    Original Revision: https://phabricator.services.mozilla.com/D325172
    
    Differential Revision: https://phabricator.services.mozilla.com/D325483

--- dom/canvas/OffscreenCanvas.h
+++ dom/canvas/OffscreenCanvas.h
@@ -135,6 +135,8 @@ class OffscreenCanvas final : public DOMEventTargetHelper,
 
   CSSIntSize GetWidthHeight() override { return CSSIntSize(mWidth, mHeight); }
 
+  bool CanCreateContext() const override { return !mNeutered; }
+
   virtual already_AddRefed<nsICanvasRenderingContextInternal> CreateContext(
       CanvasContextType aContextType) override;
 
