a9dd6fd1805607ce4d4d45ff0a5db35701f5676b
Nathan Barrett <nbarrett@mozilla.com>
2026-09-17 16:05:32 +0000

    Bug 2070977 - add hashes arg to the openNewtab function  a=pascalc
    Original Revision: https://phabricator.services.mozilla.com/D325053
    
    Differential Revision: https://phabricator.services.mozilla.com/D326950

--- browser/components/uitour/UITour.sys.mjs
+++ browser/components/uitour/UITour.sys.mjs
@@ -415,7 +415,7 @@ export var UITour = {
       }
 
       case "showNewTab": {
-        this.showNewTab(window, browser);
+        this.showNewTab(window, browser, data.hash);
         break;
       }
 
@@ -1490,9 +1490,12 @@ export var UITour = {
     }
   },
 
-  showNewTab(aWindow, aBrowser) {
+  showNewTab(aWindow, aBrowser, aHash) {
     aWindow.gURLBar.focus();
     let url = "about:newtab";
+    if (typeof aHash == "string" && /^[a-zA-Z0-9_-]+$/.test(aHash)) {
+      url += "#" + aHash;
+    }
     aWindow.openLinkIn(url, "current", {
       targetBrowser: aBrowser,
       triggeringPrincipal:
