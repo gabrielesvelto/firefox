a529de3f2f43f8e4c32b4bde7c121a182967170c
Valentin Gosu <valentin.gosu@gmail.com>
2026-09-18 07:53:24 +0000

    Bug 2061526 - Harden JAR URI deserialization  a=pascalc DONTBUILD
    Original Revision: https://phabricator.services.mozilla.com/D323545
    
    Differential Revision: https://phabricator.services.mozilla.com/D326133

--- netwerk/protocol/res/SubstitutingJARURI.h
+++ netwerk/protocol/res/SubstitutingJARURI.h
@@ -184,6 +184,9 @@ class SubstitutingJARURI : public nsIJARURI,
   bool Deserialize(const mozilla::ipc::URIParams& aParams);
   nsresult ReadPrivate(nsIObjectInputStream* aStream);
 
+  // Builds the URI aSource resolves to
+  static nsresult ResolveSource(nsIURI* aSource, SubstitutingJARURI** aResult);
+
  public:
   class Mutator final : public nsIURIMutator,
                         public BaseURIMutator<SubstitutingJARURI>,
