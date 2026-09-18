3ca27569dc138e57e1e900d426adfc2e45dcfa03
Dão Gottwald <dao@mozilla.com>
2026-09-18 07:00:48 +0000

    Bug 2070963 - Stop deriving the notification bar's top border from the toolbar separator color.  a=pascalc
    Original Revision: https://phabricator.services.mozilla.com/D325586
    
    Differential Revision: https://phabricator.services.mozilla.com/D327142

--- toolkit/themes/shared/design-system/dist/semantic-categories.mjs
+++ toolkit/themes/shared/design-system/dist/semantic-categories.mjs
@@ -1246,23 +1246,7 @@ export const tokensTable = {
       name: "--card-header-border-color-active",
     },
     {
-      value: {
-        light: "var(--color-violet-desaturated-20)",
-        dark: "var(--color-gray-60)",
-        nativeTheme: "var(--toolbarseparator-color)",
-        prefersContrast: "CanvasText",
-        default:
-          "light-dark(var(--color-violet-desaturated-20), var(--color-gray-60))",
-        platform: {
-          default: "var(--toolbarseparator-color)",
-          browserTheme: {
-            light: "var(--color-violet-desaturated-20)",
-            dark: "var(--color-gray-60)",
-            default:
-              "light-dark(var(--color-violet-desaturated-20), var(--color-gray-60))",
-          },
-        },
-      },
+      value: { default: "var(--border-color-deemphasized)" },
       name: "--info-bar-border-color",
     },
     { value: "var(--border-color-interactive)", name: "--input-border-color" },
@@ -4538,23 +4522,7 @@ export const variableLookupTable = {
   "icon-size-large": "24px",
   "icon-size-xlarge": "32px",
   "icon-size-xxlarge": "var(--size-item-xlarge)",
-  "info-bar-border-color": {
-    light: "var(--color-violet-desaturated-20)",
-    dark: "var(--color-gray-60)",
-    nativeTheme: "var(--toolbarseparator-color)",
-    prefersContrast: "CanvasText",
-    default:
-      "light-dark(var(--color-violet-desaturated-20), var(--color-gray-60))",
-    platform: {
-      default: "var(--toolbarseparator-color)",
-      browserTheme: {
-        light: "var(--color-violet-desaturated-20)",
-        dark: "var(--color-gray-60)",
-        default:
-          "light-dark(var(--color-violet-desaturated-20), var(--color-gray-60))",
-      },
-    },
-  },
+  "info-bar-border-color": { default: "var(--border-color-deemphasized)" },
   "info-bar-margin-block-start": "4px",
   "info-bar-margin-block-end": { default: "4px" },
   "info-bar-margin-inline": "4px",
