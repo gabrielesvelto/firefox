b1373f6d852fb8b9d83f8fc94226a1fdaf77032b
Leo Tenenbaum <ltenenbaum@mozilla.com>
2026-09-19 07:18:06 +0000

    Bug 2072432 - Use AppendChild instead of AppendChildTo in txMozillaXMLOutput.  a=pascalc DONTBUILD
    Original Revision: https://phabricator.services.mozilla.com/D326562
    
    Differential Revision: https://phabricator.services.mozilla.com/D327119

--- dom/xslt/xslt/txMozillaXMLOutput.cpp
+++ dom/xslt/xslt/txMozillaXMLOutput.cpp
@@ -504,7 +504,7 @@ nsresult txMozillaXMLOutput::closePrevious(bool aFlushText) {
     }
 
     ErrorResult error;
-    mCurrentNode->AppendChildTo(mOpenedElement, true, error);
+    mCurrentNode->AppendChild(*mOpenedElement, error);
     if (error.Failed()) {
       return error.StealNSResult();
     }
@@ -580,10 +580,8 @@ nsresult txMozillaXMLOutput::createTxWrapper() {
       ++j;
 #endif
     } else {
-      mDocument->RemoveChildNode(childContent, true);
-
       ErrorResult error;
-      wrapper->AppendChildTo(childContent, true, error);
+      wrapper->AppendChild(*childContent, error);
       if (error.Failed()) {
         return error.StealNSResult();
       }
@@ -597,7 +595,7 @@ nsresult txMozillaXMLOutput::createTxWrapper() {
   NS_ASSERTION(rootLocation == mDocument->GetChildCount(),
                "Incorrect root location");
   ErrorResult error;
-  mDocument->AppendChildTo(wrapper, true, error);
+  mDocument->AppendChild(*wrapper, error);
   return error.StealNSResult();
 }
 
