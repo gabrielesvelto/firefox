6226eaedfaa7a4e192f04c7ec3e8721a22c7903e
Andrew McCreight <continuation@gmail.com>
2026-09-21 09:12:20 +0000

    Bug 2059404 - Don't initialize rendering for an already-destroyed BrowserParent.  a=pascalc DONTBUILD
    If we can't send in nsFrameLoader::TryRemoteBrowserInternal() or
    BrowserParent::InitRendering(), then we've already started the process of
    destroying the BrowserParent, so don't set up this rendering stuff that we may
    have already tried to clean up.
    
    Note that mIsDestroyed is not usable here: it is only set by BrowserParent::Destroy(),
    which is not involved when the child initiates teardown.
    
    Original Revision: https://phabricator.services.mozilla.com/D323317
    
    Differential Revision: https://phabricator.services.mozilla.com/D327056

--- dom/ipc/BrowserParent.cpp
+++ dom/ipc/BrowserParent.cpp
@@ -1002,6 +1002,9 @@ void BrowserParent::ResumeLoad(uint64_t aPendingSwitchID) {
 }
 
 void BrowserParent::InitRendering() {
+  if (!CanSend()) {
+    return;
+  }
   if (mRemoteLayerTreeOwner.IsInitialized()) {
     return;
   }
