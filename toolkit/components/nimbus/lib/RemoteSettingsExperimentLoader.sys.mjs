ae785476d0dd85f3cb79af38150d5493afa8e0c8
Pascal Chevrel <pascal@mozilla.com>
2026-09-21 11:48:53 +0200

    Revert "Bug 2063425 - Don't block shutdown with failed Nimbus updates a=pascalc" for xpcshell failures a=backout
    This reverts commit c318d99e58351f38b6fdb1034f359653e1663aeb.

--- toolkit/components/nimbus/lib/RemoteSettingsExperimentLoader.sys.mjs
+++ toolkit/components/nimbus/lib/RemoteSettingsExperimentLoader.sys.mjs
@@ -390,40 +390,13 @@ export class RemoteSettingsExperimentLoader {
       this._updatingDeferred = Promise.withResolvers();
     }
 
-    try {
-      await this.withUpdateLock(() => this.#updateImpl(trigger, options));
-    } catch (e) {
-      let error;
-      if (DOMException.isInstance(e)) {
-        error = `DOMException:${e.name}`;
-      } else if (e instanceof Ci.nsIException) {
-        error = ChromeUtils.getXPCOMErrorName(e.result);
-      } else if (Error.isError(e)) {
-        try {
-          error = e.constructor.name;
-        } catch {}
-      }
-
-      if (!error) {
-        error = "(unknown)";
-      }
-
-      Glean.nimbusEvents.updateError.record({
-        error,
-        trigger,
-        during_shutdown: Services.startup.isInOrBeyondShutdownPhase(
-          Ci.nsIAppStartup.SHUTDOWN_PHASE_APPSHUTDOWNCONFIRMED
-        ),
-      });
-
-      throw e;
-    } finally {
-      this._hasUpdatedOnce = true;
-      this._updating = false;
-      this._updatingDeferred.resolve();
-    }
+    await this.withUpdateLock(() => this.#updateImpl(trigger, options));
 
     Services.prefs.setBoolPref("nimbus.firstUpdateComplete", true);
+
+    this._hasUpdatedOnce = true;
+    this._updating = false;
+    this._updatingDeferred.resolve();
 
     this.recordIsReady();
   }
