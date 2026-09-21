af7734e70983100702e4bb09a93a3c26caac4882
Lee Salzman <lsalzman@mozilla.com>
2026-09-21 19:30:24 +0000

    Bug 2072467.  a=pascalc DONTBUILD
    Original Revision: https://phabricator.services.mozilla.com/D327500
    
    Differential Revision: https://phabricator.services.mozilla.com/D327695

--- dom/canvas/OffscreenCanvas.cpp
+++ dom/canvas/OffscreenCanvas.cpp
@@ -225,6 +225,10 @@ void OffscreenCanvas::GetContext(
       aCx, contextType, aContextOptions, aRv);
   if (!result) {
     aResult.SetNull();
+    if (mDisplay) {
+      mDisplay->UpdateContext(this, nullptr, CanvasContextType::NoContext,
+                              Nothing());
+    }
     return;
   }
 
