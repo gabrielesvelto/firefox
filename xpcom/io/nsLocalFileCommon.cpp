d31f56926450d9e3d26119464e8972ffc162707e
Marco Bonardo <mbonardo@mozilla.com>
2026-09-21 11:50:48 +0000

    Bug 2067465.  a=pascalc DONTBUILD
    Original Revision: https://phabricator.services.mozilla.com/D326584
    
    Differential Revision: https://phabricator.services.mozilla.com/D327505

--- xpcom/io/nsLocalFileCommon.cpp
+++ xpcom/io/nsLocalFileCommon.cpp
@@ -116,7 +116,8 @@ const char* const sExecutableExts[] = {
   ".scf",         // Windows explorer command
   ".scr",
   ".sct",
-  ".search-ms",  // Windows Saved Search
+  ".search-ms",           // Windows Saved Search
+  ".searchConnector-ms",  // Windows Search Connector
   ".settingcontent-ms",
   ".shb",
   ".shs",
--- xpcom/io/nsLocalFileCommon.h
+++ xpcom/io/nsLocalFileCommon.h
@@ -8,9 +8,9 @@
 #define _NS_LOCAL_FILE_COMMON_H_
 
 #ifdef MOZ_ESR
-extern const char* const sExecutableExts[112];
-#else
 extern const char* const sExecutableExts[113];
+#else
+extern const char* const sExecutableExts[114];
 #endif
 
 #endif
