729dd507fbb2cd73e6712498a69431a96270bebf
Nika Layzell <nika@thelayzells.com>
2026-09-24 19:19:50 +0000

    Bug 2069395 - Lock during nsComponentManager memory reports,  a=diannaS DONTBUILD
    Original Revision: https://phabricator.services.mozilla.com/D326592
    
    Differential Revision: https://phabricator.services.mozilla.com/D327882

--- xpcom/components/nsComponentManager.cpp
+++ xpcom/components/nsComponentManager.cpp
@@ -1326,8 +1326,10 @@ nsComponentManagerImpl::CollectReports(nsIHandleReportCallback* aHandleReport,
 }
 
 size_t nsComponentManagerImpl::SizeOfIncludingThis(
-    mozilla::MallocSizeOf aMallocSizeOf) const {
+    mozilla::MallocSizeOf aMallocSizeOf) {
   size_t n = aMallocSizeOf(this);
+
+  MonitorAutoLock lock(mLock);
 
   n += mFactories.ShallowSizeOfExcludingThis(aMallocSizeOf);
   for (const auto& data : mFactories.Values()) {
