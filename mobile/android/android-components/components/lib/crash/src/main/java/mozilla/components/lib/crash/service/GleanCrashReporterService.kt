da30279ef20fba9707c9278db545e20d49d789c6
Alex Franchuk <afranchuk@mozilla.com>
2026-09-24 19:14:15 +0000

    Bug 2049749 p3 - Populate OS, OSVersion, and CPUArchitecture annotations in lib-crash  a=diannaS DONTBUILD
    Original Revision: https://phabricator.services.mozilla.com/D319599
    
    Differential Revision: https://phabricator.services.mozilla.com/D328038

--- mobile/android/android-components/components/lib/crash/src/main/java/mozilla/components/lib/crash/service/GleanCrashReporterService.kt
+++ mobile/android/android-components/components/lib/crash/src/main/java/mozilla/components/lib/crash/service/GleanCrashReporterService.kt
@@ -5,6 +5,7 @@
 package mozilla.components.lib.crash.service
 
 import android.content.Context
+import android.os.Build
 import androidx.annotation.VisibleForTesting
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.SerialName
@@ -230,6 +231,11 @@ class GleanCrashReporterService(
 
     private fun setCommonPingFields(extras: MutableMap<String, JsonElement>, crash: Crash) {
         extras.setIfAbsent(Annotation.CrashTime) { JsonPrimitive((crash.timestamp / 1000).toString()) }
+        // CrashEventID and platform info may already be present for native crashes
+        extras.setIfAbsent(Annotation.CrashEventID) { JsonPrimitive(crash.uuid) }
+        extras.setIfAbsent(Annotation.OS) { JsonPrimitive("Android") }
+        extras.setIfAbsent(Annotation.OSVersion) { JsonPrimitive("${Build.VERSION.SDK_INT}") }
+        extras.setIfAbsent(Annotation.CPUArchitecture) { JsonPrimitive(Crash.CPU_ARCH) }
     }
 
     override fun record(crash: Crash.UncaughtExceptionCrash) {
@@ -247,7 +253,6 @@ class GleanCrashReporterService(
             appBuildId?.let { extras[Annotation.BuildID] = JsonPrimitive(it) }
             extras[Annotation.JavaException] = JsonPrimitive(crash.throwable.getStacktraceAsJsonString())
             extras[Annotation.CrashType] = JsonPrimitive("uncaught exception")
-            extras[Annotation.CrashEventID] = JsonPrimitive(crash.uuid)
 
             sendCrashPing(Json.encodeToString(extras))
         }
@@ -334,8 +339,6 @@ class GleanCrashReporterService(
             extras.setIfAbsent(Annotation.CrashType) {
                 JsonPrimitive("${if (!crash.isFatal) "non-" else ""}fatal native crash")
             }
-            // CrashEventID should only be absent if there is no extras file
-            extras.setIfAbsent(Annotation.CrashEventID) { JsonPrimitive(crash.uuid) }
 
             sendCrashPing(Json.encodeToString(extras))
         }
--- mobile/android/android-components/components/lib/crash/src/test/java/mozilla/components/lib/crash/service/MozillaSocorroServiceTest.kt
+++ mobile/android/android-components/components/lib/crash/src/test/java/mozilla/components/lib/crash/service/MozillaSocorroServiceTest.kt
@@ -165,6 +165,9 @@ class MozillaSocorroServiceTest {
             assert(request.contains("name=CrashEventID\r\n\r\nf6aa9fc0-75f5-4677-b90d-7b481909365a"))
             assert(request.contains("name=CrashType\r\n\r\n$FATAL_NATIVE_CRASH_TYPE"))
             assert(request.contains("name=CrashTime\r\n\r\n123"))
+            assert(request.contains("name=OS\r\n\r\nAndroid"))
+            assert(request.contains("name=OSVersion\r\n\r\n"))
+            assert(request.contains("name=CPUArchitecture\r\n\r\n"))
             assert(request.contains("name=useragent_locale\r\n\r\nen-US"))
             assert(request.contains("name=Breadcrumbs\r\n\r\n[{\"timestamp\":\"2018-06-12T19:30:00\",\"message\":\"Hello World\",\"category\":\"\",\"level\":\"Debug\",\"type\":\"Default\",\"data\":{}}]"))
 
@@ -514,6 +517,9 @@ class MozillaSocorroServiceTest {
             assert(request.contains("name=CrashEventID\r\n\r\n13b041a0-3958-4d11-b3b6-194c973d8d58"))
             assert(request.contains("name=CrashType\r\n\r\n$UNCAUGHT_EXCEPTION_TYPE"))
             assert(request.contains("name=CrashTime\r\n\r\n123"))
+            assert(request.contains("name=OS\r\n\r\nAndroid"))
+            assert(request.contains("name=OSVersion\r\n\r\n"))
+            assert(request.contains("name=CPUArchitecture\r\n\r\n"))
             assert(request.contains("name=useragent_locale\r\n\r\nen-US"))
 
             verify(service).report(crash)
